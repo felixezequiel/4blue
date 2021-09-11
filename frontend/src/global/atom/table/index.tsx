59bb176b890dd949c9574509c2f533b929044f33
felixezequiel <felixezequiel48@gmail.com>
2021-09-11 19:02:33 -0300

    continue project

--- frontend/src/global/atom/table/index.tsx
+++ frontend/src/global/atom/table/index.tsx
@@ -1,36 +1,38 @@
+import { TableCustom, TdCustom, ThCustom, TheadCustom, TrCustom } from "./style"
+
 interface TableProps { 
   view: Array<string>, 
   [key: string]: any 
 }
 export const Table = ({ data, view, callback }: TableProps) => {
   return (
-    <table>
-      <thead>
-        <tr>
+    <TableCustom>
+      <TheadCustom>
+        <TrCustom>
           {/* Obtem as colunas do cabecalho da tabela com base nas keys do objeto json */}
           {Object.keys(data[0] || []).filter(key => !view.length || view.indexOf(key) !== -1).map((keys, index) => (
-            <th key={ index }>
+            <ThCustom key={ index }>
               { keys }
-            </th>
+            </ThCustom>
           ))}
-        </tr>
-      </thead>
+        </TrCustom>
+      </TheadCustom>
       <tbody>
         {/* Retorna uma linha para cada posicao do array */}
         {data.map((dt: any, key: number) => (
-          <tr 
+          <TrCustom 
             key={ key }
             onClick={ () => callback(dt.id) }
           >
             {/* Cria as colunas de cada linha com base nas chaves do objeto json */}
             {Object.keys(dt).filter(key => !view.length || view.indexOf(key) !== -1).map((keys, index) => (
-              <td key={ index }>
+              <TdCustom key={ index }>
                 { dt[keys] }
-              </td>
+              </TdCustom>
             ))}
-          </tr>
+          </TrCustom>
         ))}
       </tbody>
-    </table>
+    </TableCustom>
   )
 }
---
+++ frontend/src/global/atom/table/style/index.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const TableCustom = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`
+
+export const TheadCustom = styled.thead`
+  background-color: #1f1f1f;
+`
+
+export const TrCustom = styled.tr`
+  border-bottom: 1px solid gray;
+  cursor: pointer;
+`
+
+export const ThCustom = styled.th`
+  font-weight: bolder;
+  color: white;
+  padding: 15px;
+  text-align: left;
+`
+
+export const TdCustom = styled.td`
+  padding: 7px;
+`
