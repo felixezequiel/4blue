a8f7e4467be004cf231f61184d4bd09ca1abaef2
felixezequiel <felixezequiel48@gmail.com>
2021-09-12 18:19:51 -0300

    stetic

--- frontend/src/provider/socket/index.tsx
+++ frontend/src/provider/socket/index.tsx
@@ -1,7 +1,7 @@
 import { createContext } from "react"
 import socket from 'socket.io-client'
 import { ChildrenProps } from "../../types"
-const io = socket('http://localhost:3031', {autoConnect: true, })
+const io = socket('http://localhost:3031', { autoConnect: true, })
 
 export const SocketIo = createContext(io)
 export const Socket = ({ children }: ChildrenProps) => {
